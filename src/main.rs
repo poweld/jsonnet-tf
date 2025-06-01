5172776b2ec2032ef6e64b3d4613d4e54147b3f7
Dave Powell <poweld@gmail.com>
2025-06-01 09:36:06 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::process::Command;
 use std::env;
 use std::str;
 use uuid::Uuid;
-use std::path;
 use std::fs;
 
 fn main() {
@@ -14,16 +13,15 @@ fn main() {
   let output = str::from_utf8(&output.stdout)
     .expect("failed to convert output to str");
   println!("{output}");
-  //let tmp = mktemp();
-  mktemp();
+  let tmpdir = mktemp().expect("failed to create tmp dir");
+  let _ = fs::remove_dir_all(tmpdir);
 }
 
-fn mktemp() { // -> std::io::Result<Path> {
+fn mktemp() -> std::io::Result<String> {
   let os_tmp = env::temp_dir();
   let os_tmp_str = os_tmp.display();
-  // println!("Temporary directory: {}", osTmp.display());
   let id = Uuid::new_v4();
-  println!("{:?}", id);
-  println!("{os_tmp_str}/{id}");
-  fs::create_dir("{osTmp}/{id}");
+  let temp = format!("{os_tmp_str}/{id}");
+  fs::create_dir(&temp)?;
+  Ok(temp)
 }
